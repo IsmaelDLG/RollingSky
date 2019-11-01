a672b554e3a0e41e9ef4d36ca2bdf901ae977749
IsmaelDLG <48798165+IsmaelDLG@users.noreply.github.com>
2019-11-01 11:15:08 +0100

    Test Scene creat.
    Moviment bàsic del jugador fet.
      - Ara el jugador es mou cap endevant a una velocitat creixent fins a un màxim. Hi ha un màxim superior per quan s'accelera.
      - Ara el jugador es pot moure (molt primitivament) de dreta a esquerra.
    
    Càmera segueix al jugador amb un offset fix i una rotació determinada.

---
+++ Rollin'Sky/Assets/Scripts/Camera.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    private Transform tr;
+    public GameObject pc;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        tr = GetComponent<Transform>();
+        //Initialize vars
+        tr.Rotate(new Vector3(30.0f, 0.0f, 0.0f));
+
+        //get components
+
+        tr.SetPositionAndRotation(this.watchPlayer(pc.transform.position),tr.rotation);  
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        tr.SetPositionAndRotation(this.watchPlayer(pc.transform.position), tr.rotation);
+    }
+
+    //My methods
+    public Vector3 watchPlayer(Vector3 playerPos)
+    {
+        return playerPos + new Vector3(0.0f, 5.0f, -4.5f);
+    }
+
+}
---
+++ Rollin'Sky/Assets/Scripts/Player.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public const float MAX_SPEED = 10.0f;
+    public const float NORMAL_SPEED = 5.0f;
+    public const float MIN_SPEED = 1.0f;
+
+    private Rigidbody rb;
+    private Transform tr;
+
+    private Vector3 speed;
+    private Vector3 accelertion;
+
+   
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+
+        //Agafo els components que te el player
+        tr = this.transform;
+        rb = this.GetComponent<Rigidbody>();
+
+        //Inicialitzo aquests components
+        tr.SetPositionAndRotation(new Vector3(0, 2, 1), tr.rotation);
+        rb.useGravity = true;
+
+        //Acceleracio hardcoded
+        accelertion = new Vector3(0.0f, 0.0f, 0.25f);
+
+        //Velocitat inicial
+        speed = new Vector3(0.0f, 0.0f, MIN_SPEED);
+
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        //Calcul de velocitat puntual en l'eix z (v + v*a);
+        speed = speed + new Vector3(speed.x * accelertion.x * Time.deltaTime,
+            speed.y * accelertion.y * Time.deltaTime, speed.z * accelertion.z * Time.deltaTime);
+       
+        //Ajustament de velocitat en eix z
+        if (speed.z > NORMAL_SPEED) speed.z = MAX_SPEED;
+
+        //Calcul de vel en eix x
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            speed += new Vector3(0.05f, 0.0f, 0.0f);
+            Debug.Log("Right Arrow pressed!");
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            speed += new Vector3(-0.05f, 0.0f, 0.0f);
+            Debug.Log("Left Arrow pressed!");
+        }
+
+        rb.AddForce(speed);
+    }
+
+    //My methods
+
+    public Vector3 getPosition()
+    {
+        return tr.position;
+    }
+}
