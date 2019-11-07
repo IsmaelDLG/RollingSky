9ae18afccd05e59d3fa6c5882303e63fb7ec8d83
IsmaelDLG <48798165+IsmaelDLG@users.noreply.github.com>
2019-11-07 06:42:38 +0100

    read txt file

---
+++ Rollin'Sky/Assets/Scripts/LevelCtl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+public class LevelCtl : MonoBehaviour
+{
+    public const string path = "/Scripts/LevelMap/test_0.txt";
+    public GameObject pc;
+
+    string[] level;
+    int width;
+    // Start is called before the first frame update
+    void Start()
+    {
+        string mypath = Application.dataPath + path;
+        Debug.Log(mypath);
+        /*using (var fileStream = new FileStream(@, FileMode.Open, FileAccess.Read))
+        {
+            // read from file
+        }*/
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
