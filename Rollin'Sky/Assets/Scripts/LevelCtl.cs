d343ce3a8979636c646221a40ed9b58ac329d047
IsmaelDLG <ismadlg.works@gmail.com>
2019-11-08 14:09:13 +0100

    tmp

--- Rollin'Sky/Assets/Scripts/LevelCtl.cs
+++ Rollin'Sky/Assets/Scripts/LevelCtl.cs
@@ -9,18 +9,36 @@ public class LevelCtl : MonoBehaviour
     public const string path = "/Scripts/LevelMap/test_0.txt";
     public GameObject pc;
 
+    public List<GameObject> tiles;
+    public List<GameObject[]> obstacles;
+
     string[] level;
     int width;
     // Start is called before the first frame update
     void Start()
     {
+        //Llegeixo el meu nivell
         string mypath = Application.dataPath + path;
-        Debug.Log(mypath);
-        /*using (var fileStream = new FileStream(@, FileMode.Open, FileAccess.Read))
-        {
-            // read from file
-        }*/
+        string file = File.ReadAllText(mypath);
+        int rows = file.Split(';').Length;
 
+        //Carrego els tiles disponibles
+        GameObject myTile = new GameObject();
+        for (int it = 0; myTile != null; it++)
+        {
+            //find returns null if it couldn't find the obstacle
+            myTile = GameObject.Find("Tile_" + it.ToString());
+            tiles.Add(myTile);
+        }
+
+        //Carrego els obstacles disponibles
+        GameObject myObstacle = new GameObject();
+        for (int it = 0; myObstacle != null; it++)
+        {
+            //find returns null if it couldn't find the obstacle
+            myTile = GameObject.Find("Obstacle_" + it.ToString());
+            tiles.Add(myTile);
+        }
     }
 
     // Update is called once per frame
