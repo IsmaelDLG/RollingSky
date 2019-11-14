cd969da349dba700b9dc479af3a87530fd7b5e1a
EnricSegura <enricsegurasanchez@gmail.com>
2019-11-14 21:40:38 +0100

    Menu de pausa fet (quan tanques el menu el joc s'accelera)

---
+++ Rollin'Sky/Assets/Scripts/PauseMenuController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuController : MonoBehaviour
+{
+    public static bool IsPaused = false;
+    public GameObject pauseUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(IsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resume()
+    {
+        pauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        IsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseUI.SetActive(true);
+        Time.timeScale = 0;
+        IsPaused = true;
+    }
+
+    public void loadMainMenu()
+    {
+        IsPaused = false;
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menus");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exit Completed");
+        Application.Quit();
+    }
+}
