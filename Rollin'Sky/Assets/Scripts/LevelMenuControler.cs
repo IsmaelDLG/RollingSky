58f87923b4ec204582bea8d4c07d024230f0765f
EnricSegura <enricsegurasanchez@gmail.com>
2019-11-14 20:38:46 +0100

    Tots els assets necessaris

---
+++ Rollin'Sky/Assets/Scripts/LevelMenuControler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelMenuControler : MonoBehaviour
+{
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("TestModel");
+    }
+}
---
+++ Rollin'Sky/Assets/Scripts/MainMenuControler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuControler : MonoBehaviour
+{
+
+    public void ExitGame()
+    {
+        Debug.Log("Exit Complete");
+        Application.Quit();
+    }
+
+}
