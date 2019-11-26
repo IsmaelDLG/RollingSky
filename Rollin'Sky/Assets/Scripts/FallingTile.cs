9fdd08ddcd8ce3c525c176c0d6ca69182f36b053
EnricSegura <enricsegurasanchez@gmail.com>
2019-11-26 20:02:50 +0100

    Tile de caiguda

---
+++ Rollin'Sky/Assets/Scripts/FallingTile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingTile : MonoBehaviour
+{
+
+    public bool startFalling = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            startFalling = true;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (startFalling)
+        {
+            this.transform.Translate(new Vector3(0.0f, -0.01f, 0.0f));
+        }
+    }
+}
