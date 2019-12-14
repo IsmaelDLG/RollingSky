83c4e6b92d20f661948cf8b068cc2eaac52c86fe
EnricSegura <enricsegurasanchez@gmail.com>
2019-12-14 21:19:17 +0100

    GodMode + nivell 3 i canons colocats

---
+++ Rollin'Sky/Assets/Scripts/GodCtrl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GodCtrl : MonoBehaviour
+{
+    public GameObject player;
+
+    // Update is called once per frame
+
+    void deactivateGodMode()
+    {
+        this.GetComponent<BoxCollider>().isTrigger = true;
+    }
+
+    void activateGodMode()
+    {
+        this.GetComponent<BoxCollider>().isTrigger = false;
+    }
+    void Update()
+    {
+        if(player.GetComponent<PlayerMove>().godMode)
+        {
+            activateGodMode();
+        }
+        else
+        {
+            deactivateGodMode();
+        }
+    }
+}
