ca3b364ef85d3fcf48f0c697ee95f7ed7bb7e8cf
EnricSegura <enricsegurasanchez@gmail.com>
2019-12-14 18:03:02 +0100

    cano

---
+++ Rollin'Sky/Assets/Scripts/BulletBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour
+{
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(new Vector3(0.025f, 0.0f, 0.0f));
+    }
+}
