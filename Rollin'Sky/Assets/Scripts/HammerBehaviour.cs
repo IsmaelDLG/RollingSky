0f79768f99c337bd3f77f1f1a5640d6d0238147a
IsmaelDLG <48798165+IsmaelDLG@users.noreply.github.com>
2019-12-15 11:04:15 +0100

    hammer prefab & hammer behaviour finished

---
+++ Rollin'Sky/Assets/Scripts/HammerBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HammerBehaviour : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(new Vector3(0, 5, 0));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.gameObject.GetComponent<PlayerMove>().killPlayer();
+        }
+    }
+}
