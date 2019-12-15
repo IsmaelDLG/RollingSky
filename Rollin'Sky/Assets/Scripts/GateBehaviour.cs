ca2595e1ff4f8524c9b5b2e216456129f49a324f
EnricSegura <enricsegurasanchez@gmail.com>
2019-12-15 21:12:47 +0100

    gate behaviour

---
+++ Rollin'Sky/Assets/Scripts/GateBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateBehaviour : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (other.gameObject.GetComponent<PlayerMove>().element != "lava")
+            {
+                other.gameObject.GetComponent<PlayerMove>().killPlayer();
+            }
+        }
+    }
+}
